b48c60e704d914865877edc2628e08fce02faf95
JAMIE COVELL <jc217189@falmouth.ac.uk>
2021-11-12 16:46:03 +0000

    Summary added to all mathl and extensions

--- Assets/Scripts/Utility/Extensions.cs
+++ Assets/Scripts/Utility/Extensions.cs
@@ -6,10 +6,24 @@ public static class Extensions
 {
     #region Number
 
+    /// <summary>
+    /// Is int x between the min and max values
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
     public static bool Between(this int x, int min, int max)
     {
         return ((x - min) | (max - x)) >= 0;
     }
+    /// <summary>
+    /// Is float x between the min and max values
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
     public static bool Between(this float x, float min, float max)
     {
         return (x >= min) && (x <= max);
@@ -19,22 +33,43 @@ public static class Extensions
 
     #region Vector
 
+    /// <summary>
+    /// Returns the vector v with the X value set to zero
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
     public static Vector3 FlattenX(this Vector3 v)
     {
         v.x = 0;
         return v;
     }
+    /// <summary>
+    /// Returns the vector v with the Y value set to zero
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
     public static Vector3 FlattenY(this Vector3 v)
     {
         v.y = 0;
         return v;
     }
+    /// <summary>
+    /// Returns the vector v with the Z value set to zero
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
     public static Vector3 FlattenZ(this Vector3 v)
     {
         v.z = 0;
         return v;
     }
 
+    /// <summary>
+    /// Returns vector a pointwise multiplied by vector b
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
     public static Vector3 Times(this Vector3 a, Vector3 b)
     {
         a.x *= b.x;
@@ -43,25 +78,51 @@ public static class Extensions
         return a;
     }
 
+    /// <summary>
+    /// Casts the vector2 a to a vector3 with the x, y mapped to x, z
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
     public static Vector3 ToVector3(this Vector2 a)
     {
         return new Vector3(a.x, 0, a.y);
     }
 
+    /// <summary>
+    /// Returns the vector3 a as a vector2 with x, y mapped to x, y
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
     public static Vector2 ToVector2XY(this Vector3 a)
     {
         return new Vector2(a.x, a.y);
     }
 
+    /// <summary>
+    /// Returns the vector3 a as a vector2 with x, z mapped to x, y
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
     public static Vector2 ToVector2XZ(this Vector3 a)
     {
         return new Vector2(a.x, a.z);
     }
 
+    /// <summary>
+    /// Returns a random unit Vector3
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
     public static Vector3 Randomise(this Vector3 a)
     {
         return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
+    /// <summary>
+    /// Returns a random unit Vector3 scaled by a magnitude
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="_magnitude"></param>
+    /// <returns></returns>
     public static Vector3 Randomise(this Vector3 a, float _magnitude)
     {
         return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * _magnitude;
@@ -71,6 +132,11 @@ public static class Extensions
 
     #region Quaternion
 
+    /// <summary>
+    /// Returns a random quaternion
+    /// </summary>
+    /// <param name="q"></param>
+    /// <returns></returns>
     public static Quaternion Randomise(this Quaternion q)
     {
         return new Quaternion(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
--- Assets/Scripts/Utility/Mathl.cs
+++ Assets/Scripts/Utility/Mathl.cs
@@ -7,27 +7,66 @@ using UnityEngine;
 /// </summary>
 public static class Mathl
 {
-    
+    /// <summary>
+    /// Its Mathf.Sin
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
     public static float Sin(float x)
     {
         return Mathf.Sin(x);
     }
+    /// <summary>
+    /// Mathf.Sin With variables to tweak
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="amplitude"></param>
+    /// <param name="frequency"></param>
+    /// <param name="xOffset"></param>
+    /// <param name="yOffset"></param>
+    /// <returns></returns>
     public static float Sin(float x, float amplitude, float frequency, float xOffset, float yOffset)
     {
         return amplitude * Sin(frequency * x + xOffset) + yOffset;
     }
+    /// <summary>
+    /// Mathf.Sin but with one peak rooted at 0, 1
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
     public static float NormalisedSin(float x)
     {
         return Sin(x * Mathf.PI);
     }
+    /// <summary>
+    /// Sin rooted at 0, 1 with tweakable variables
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="amplitude"></param>
+    /// <param name="frequency"></param>
+    /// <param name="xOffset"></param>
+    /// <param name="yOffset"></param>
+    /// <returns></returns>
     public static float NormalisedSin(float x, float amplitude, float frequency, float xOffset, float yOffset)
     {
-        return amplitude * Sin(frequency * x + xOffset) + yOffset;
+        return amplitude * Sin(frequency * x * Mathf.PI + xOffset) + yOffset;
     }
+    /// <summary>
+    /// Sin with directional skew for varied attack and decay
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="skew"></param>
+    /// <param name="dir"></param>
+    /// <returns></returns>
     public static float SkewedSin(float x, float skew, int dir)
     {
         return NormalisedSin(Mathf.PI * x + (Sin(x)*(skew/dir))) * dir;
     }
+    /// <summary>
+    /// A nice Sin wave for a bit of wobble at either end
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
     public static float WobbleSin(float x)
     {
         return 0.33f * Sin(2.621f * Mathf.PI * x + -Mathf.PI) + (1.314f * x);
